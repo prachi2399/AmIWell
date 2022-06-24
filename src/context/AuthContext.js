2813d53a188bc8316726302e52089518f1962395
prachi2399 <prachi9239soni@gmail.com>
2022-06-24 20:49:18 +0530

    Create AuthContext.js

---
+++ src/context/AuthContext.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { auth } from "../firebase";
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return React.useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  function logout() {
+    return auth.signOut();
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+
+  useEffect(() => {
+    const unsubscibe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscibe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signup,
+    login,
+    logout,
+    resetPassword,
+  };
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
